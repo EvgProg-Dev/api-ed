08b13611652c164b43b943ac68b6af34d80f9ced
Evgeniy <evg1999prog@gmail.com>
2025-02-09 22:00:30 +0200

    TEST

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,10 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!!!!");
 });
 
+app.get("/users", (req: Request, res: Response) => {
+    res.send("USERS!!!!!");
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
